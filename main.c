105dcd311aa541263fe61f6338c18ffdbf1a40d9
Hardik01363 <hardikkhandelwal444@gmail.com>
2026-07-12 13:07:42 +0530

    Set-up basic structure of code

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+void shell_loop(char** env) {
+
+}
+
+int main(int argc, char** argv, char** env) {
+    //we wont need argc and argv
+    (void)argc;
+    (void)argv;
+    
+    shell_loop(env);
+
+    return 0;
+}
